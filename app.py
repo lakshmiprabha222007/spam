470e193794c4731e56a762e79a4adc07ebdbe148
lakshmiprabha222007 <lakshmiprabha222007@gmail.com>
2025-12-14 13:09:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pickle
 
-# Load trained pipeline model
+# Load model
 with open("trained_spam_classifier_model.pkl", "rb") as f:
     model = pickle.load(f)
+
+# Load vectorizer
+with open("vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
 
 st.title("Spam Message Classifier")
 
@@ -13,9 +17,12 @@ if st.button("Predict"):
     if message.strip() == "":
         st.warning("Please enter a message")
     else:
-        prediction = model.predict([message])
+        # Convert text → numeric features
+        message_vector = vectorizer.transform([message])
 
-        if prediction[0] == 1 or prediction[0] == "spam":
+        prediction = model.predict(message_vector)
+
+        if prediction[0] == 1:
             st.error("🚫 Spam Message")
         else:
             st.success("✅ Not Spam Message")
