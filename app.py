c726533a1e1f13768c5ab89c45f73bd3b6ac9926
lakshmiprabha222007 <lakshmiprabha222007@gmail.com>
2025-12-14 12:19:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import streamlit as st
 import pickle
 
-# Load trained spam classifier model
 with open("trained_spam_classifier_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("📩 Spam Message Classifier")
 
-st.write("Enter a message to check whether it is Spam or Not Spam")
-
-# Text input
 message = st.text_area("Enter Message")
 
 if st.button("Predict"):
     if message.strip() == "":
         st.warning("Please enter a message")
     else:
-        prediction = model.predict([message])
+        result = model.predict([message])[0]
 
-        if prediction[0] == 1:
+        if result == 1:
             st.error("🚫 SPAM MESSAGE")
         else:
             st.success("✅ NOT SPAM")
